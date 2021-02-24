21c94ef5fad7f3d1d1ad534da06ee5625f25b1d0
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-02-24 10:48:25 +0100

    robot

--- ui.js
+++ ui.js
@@ -1,11 +1,43 @@
 'use strict';
 const React = require('react');
-const {Text} = require('ink');
+const {useInput, useApp, Box, Text} = require('ink');
 
-const App = ({name = 'Kaat'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
-);
+const Robot = () => {
+	const {exit} = useApp();
+	const [x, setX] = React.useState(1);
+	const [y, setY] = React.useState(1);
 
-module.exports = App;
+	useInput((input, key) => {
+		if (input === 'q') {
+			exit();
+		}
+
+		if (key.leftArrow) {
+			setX(Math.max(1, x - 1));
+		}
+
+		if (key.rightArrow) {
+			setX(Math.min(20, x + 1));
+		}
+
+		if (key.upArrow) {
+			setY(Math.max(1, y - 1));
+		}
+
+		if (key.downArrow) {
+			setY(Math.min(10, y + 1));
+		}
+	});
+
+	return (
+		<Box flexDirection="column">
+			<Text>Use arrow keys to move the face. Press “q” to exit.</Text>
+			<Box height={12} paddingLeft={x} paddingTop={y}>
+				<Text>^_^</Text>
+			</Box>
+		</Box>
+	);
+};
+
+
+module.exports = Robot;
